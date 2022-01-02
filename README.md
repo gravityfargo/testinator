f06939a873e9f1824549e670ccfb2db3a9279dba
RRFSS <rmrfforewardslashstar@gmail.com>
2022-01-02 08:55:36 -0800

    It does what I need it to now

--- README.md
+++ README.md
@@ -1,8 +1,14 @@
-I needed a program to save me time while working on GNOME extensions so I made this. 
+I needed a program to save me time while working on GNOME extensions so I made this. I use a [makefile](https://github.com/julio641742/extend-panel-menu/blob/4fa4e4fb6b87c81e38dfd83c1d9efe41cfb2dd08/Makefile) to build my extensions, hence the command feature. 
+
+![This is an image](https://imgur.com/9huY2y7)
 
 Features:
-- Restarts GNOME (X11 only)
+- enable / disable / remove extension by UUID
+- list extensions
 - Sends a test system notification
+- run a terminal command
+- Restarts GNOME (X11 only)
 
-run 
+
+run
 `gjs testinator.js`
--- testinator.js
+++ testinator.js
@@ -1,6 +1,8 @@
 imports.gi.versions.Gtk = "3.0";
 const { Gio, Gtk, Notify, Gdk, GLib } = imports.gi;
 
+
+
 class TestinatorWindow {
   constructor(app) {
     this._app = app;
@@ -12,43 +14,186 @@ class TestinatorWindow {
     this.guiRestartCommand = null;
   }
   _buildUI() {
+
     this._window = new Gtk.ApplicationWindow({
       application: this._app,
       defaultHeight: 200,
       defaultWidth: 200,
     });
+
     this._box = new Gtk.Box({
       orientation: Gtk.Orientation.VERTICAL,
     });
 
-    //WM Restarter
-    const guiRestartButton = new Gtk.Button({ label: "Restart GNOME" });
-    guiRestartButton.connect("clicked", () => {
-      log(
-        "[Testinator] - GNOME Restarted ------------------------------------------------------------------------------------------------------------------------------------------"
-      );
-      this.guiRestarter();
+    this.extensionUUIDLabel = new Gtk.Label({ label: "Enter Extension UUID"});
+    this.extensionUUIDField = new Gtk.Entry({
+      buffer: new Gtk.EntryBuffer()
     });
-    this._box.add(guiRestartButton);
 
-    //Notifcation Tester
+
+
+
+
+    this.toggleGrid = new Gtk.Grid()
+    this.extensionEnableButton = new Gtk.Button({
+      label: "Enable Extension"
+    });
+    this.extensionEnableButton.connect("clicked", () => {
+      if (this.extensionUUIDField.get_buffer().text === "") {
+        log(
+          "[Testinator] - No UUID was provided! ------------------------------------------------------------------------------------------------------------------------------------------"
+        );
+        this.alertLabel.set_text("No UUID was provided!")
+      } else {
+        this.command("gnome-extensions enable " + this.extensionUUIDField.get_buffer().text );
+
+      }
+    });
+    this.disableExtensionButton = new Gtk.Button({
+      label: "Disable Extension"
+    });
+    this.disableExtensionButton.connect("clicked", () => {
+      if (this.extensionUUIDField.get_buffer().text === "") {
+        log(
+          "[Testinator] - No UUID was provided! ------------------------------------------------------------------------------------------------------------------------------------------"
+        );
+        this.alertLabel.set_text("No UUID was provided!")
+      } else {
+        this.command("gnome-extensions disable " + this.extensionUUIDField.get_buffer().text );
+
+      }
+    });
+
+
+
+
+
+    this.extensionRemoveButton = new Gtk.Button({
+      label: "Remove Extension"
+    });
+    this.extensionRemoveButton.connect("clicked", () => {
+      if (this.extensionUUIDField.get_buffer().text === "") {
+        log(
+          "[Testinator] - No UUID was provided! ------------------------------------------------------------------------------------------------------------------------------------------"
+        );
+        this.alertLabel.set_text("No UUID was provided!")
+      } else {
+        this.command("gnome-extensions uninstall " + this.extensionUUIDField.get_buffer().text );
+
+      }
+    });
+
+
+
+
+
+    this.listExtensions = new Gtk.Button({
+      label: "List Extensions"
+    });
+    this.listExtensions.connect("clicked", () => {
+
+        this.command("gnome-extensions list");
+    });
+
+
+
+
+
+    this.testNotifyButton = new Gtk.Button({
+      label: "Send Test System Notification"
+    });
     Notify.init("Notification Init");
     var notification = new Notify.Notification({
       summary: "Notification Summary",
       body: "Notification Body",
       "icon-name": "dialog-information",
     });
-    const notifyButton = new Gtk.Button({ label: "Send Test Notification" });
-    notifyButton.connect("clicked", () => {
+    this.testNotifyButton.connect("clicked", () => {
       log(
-        "[Testinator] - Notification Sent ------------------------------------------------------------------------------------------------------------------------------------------"
+        "[Testinator] - Notification Sent! ------------------------------------------------------------------------------------------------------------------------------------------"
       );
       notification.show();
     });
-    this._box.add(notifyButton);
+    
+    
+    
+    
+    
+    this.commandLabel = new Gtk.Label({
+      label: "Enter Command"
+    });
+    this.commandGrid = new Gtk.Grid();
+    this.commandField = new Gtk.Entry({
+      buffer: new Gtk.EntryBuffer()
+    });
+    this.commandButton = new Gtk.Button({
+      label: "Run Command"
+    });
+    this.commandButton.connect("clicked", () => {
+      if (this.commandField.get_buffer().text === "") {
+        log(
+          "[Testinator] - No command was provided! ------------------------------------------------------------------------------------------------------------------------------------------"
+        );
+        this.alertLabel.set_text("No command was provided!")
+      }
+      this.alertLabel.set_text("")
+      log(this.commandField.get_buffer().text);
+      this.command(this.commandField.get_buffer().text);
+    })
+
+
+
+
+
+    this.restartGNOME = new Gtk.Button({
+      label: "Restart GNOME Shell"
+    });
+    this.restartGNOME.connect("clicked", () => {
+      log(
+        "[Testinator] - GNOME Restarted! ------------------------------------------------------------------------------------------------------------------------------------------"
+      );
+      this.command("busctl --user call org.gnome.Shell /org/gnome/Shell org.gnome.Shell Eval s 'Meta.restart(\"Restarting…\")'");
+    })
+
+
+
+
+
+    this.alertLabel = new Gtk.Label({
+      label: ""
+    });
+
+
+
+
+    this._box.add(this.extensionUUIDLabel);
+    this._box.add(this.extensionUUIDField);
+    this._box.add(this.toggleGrid);
+    this.toggleGrid.attach(this.extensionEnableButton, 0, 0, 1, 1);
+    this.toggleGrid.attach(this.disableExtensionButton, 1, 0, 1, 1);
+    this._box.add(this.extensionRemoveButton);
+    this._box.add(this.listExtensions);
+    this._box.add(this.testNotifyButton);
+    this._box.add(this.commandLabel);
+    this._box.add(this.commandGrid);
+    this._window.add(this._box);
+    this.commandGrid.attach(this.commandField, 0, 0, 1, 1);
+    this.commandGrid.attach(this.commandButton, 1, 0, 1, 1);
+    this._box.add(this.restartGNOME);
+    this._box.add(this.alertLabel);
+
+
+
+
+
+
+
+
+    //Notifcation Tester
+
+    //this._box.add(notifyButton);
     this._box.show_all();
 
-    this._window.add(this._box);
   }
 
   getWidget() {
@@ -56,10 +201,8 @@ class TestinatorWindow {
     return this._window;
   }
 
-  guiRestarter() {
-    GLib.spawn_command_line_async(
-      "busctl --user call org.gnome.Shell /org/gnome/Shell org.gnome.Shell Eval s 'Meta.restart(\"Restarting…\")'"
-    );
+  command(command) {
+    GLib.spawn_command_line_async(command);
   }
 }
 
